fb66e9b764fddbae90bd9eaf161c91794dd94560
Barbe-Bleue <screy@icloud.com>
2018-12-11 23:51:19 +0100

    update gender function

--- commands/gender.js
+++ commands/gender.js
@@ -2,8 +2,7 @@ const axios = require('axios');
 
 module.exports = async args => {
 	let res = await axios.get("https://gender-api.com/get?name="+args[0]+"&country=FR&key=kXRfKPCeGsNKcUwseW")
-	
 	let emoji = res.data.gender === "male" ? ":man:" : ":woman:"
-	
-	return res.data.name_sanitized+': '+res.data.gender + emoji +" sûr à " + res.data.accuracy + "%"
+	let gender = res.data.gender === "male" ? "homme" : "femme"
+	return res.data.name_sanitized+': '+gender + emoji +" sûr à " + res.data.accuracy + "%"
 }
