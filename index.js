f4a9d3ef6e29d40c285b11829ee1f7d1b614dc55
Barbe-Bleue <screy@icloud.com>
2018-12-11 18:28:34 +0100

    remove comment

--- index.js
+++ index.js
@@ -657,15 +657,3 @@ bot.on('message', message => {
 });
 
 bot.login(token);
-
-/*
-ressources API :
-https://github.com/pgrimaud/horaires-ratp-api
-https://openweathermap.org/api
-http://thedogapi.co.uk/api/v1/dog?limit=1
-https://developers.giphy.com/dashboard/
-https://github.com/jprichardson/node-google
-https://www.npmjs.com/package/rss-to-json
-https://translate.yandex.net/api/v1.5/tr.json/translate?key=&text=&lang=&format=plain
-https://www.nexmo.com/
-*/
