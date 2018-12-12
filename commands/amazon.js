fcb676e79e6632506242bf743d3e1ec9f7f5d818
Barbe-Bleue <screy@icloud.com>
2018-12-12 11:12:04 +0100

    var to let

--- commands/amazon.js
+++ commands/amazon.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports = search => {	
-	var url = "https://www.amazon.fr/s/ref=nb_sb_noss?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords="+search;
+	let url = "https://www.amazon.fr/s/ref=nb_sb_noss?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords="+search;
 	return(new Discord.RichEmbed()
 		.setTitle("Recherche amazon pour: "+search)
 		.setColor(0xF3A847)
