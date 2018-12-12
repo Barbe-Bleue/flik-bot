d0e3cd0f2dc9444cbfea0cd498a07b21bc3d7bcc
Barbe-Bleue <screy@icloud.com>
2018-12-12 01:39:50 +0100

    udpate police

--- commands/insult/police.js
+++ commands/insult/police.js
@@ -1,13 +1,11 @@
 const insult = require ('./insults.json');
 
 module.exports = message => {
-  if(insult['insultes'].filter(item => message.content.includes(item)).length >= 1) {
-    let police = ':oncoming_police_car: :rotating_light: POLICE DES GROS MOTS :rotating_light: :oncoming_police_car:';
+  if(!message.member.user.bot && insult['insultes'].filter(item => message.content.toLowerCase().includes(item)).length >= 1) {
     let kickable = message.member.kickable ? true : false
-    let msg = kickable ? "T'es mort fdp" : "Ohw c'est vous admin ? Excuser moi pour le dérangement"
     return {
-      police: police,
-      msg: msg,
+      police: ':oncoming_police_car: :rotating_light: POLICE DES GROS MOTS :rotating_light: :oncoming_police_car:',
+      msg: kickable ? "pd va" : "Ohw c'est vous admin ? Excuser moi pour le dérangement",
       mutable: kickable
     }
   }
