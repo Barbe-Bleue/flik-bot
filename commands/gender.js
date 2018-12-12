bfff9a533c7add729a1af1c0ada9f5e6ebebc5cb
Barbe-Bleue <screy@icloud.com>
2018-12-12 11:11:54 +0100

    move api key (free key)

--- commands/gender.js
+++ commands/gender.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
-
+const genderApiKey = require('../config.json').genderApiKey
 module.exports = async args => {
-	let res = await axios.get("https://gender-api.com/get?name="+args[0]+"&country=FR&key=kXRfKPCeGsNKcUwseW")
+	let res = await axios.get("https://gender-api.com/get?name="+args[0]+"&country=FR&key="+genderApiKey)
 	let emoji = res.data.gender === "male" ? ":man:" : ":woman:"
 	let gender = res.data.gender === "male" ? "homme" : "femme"
 	return res.data.name_sanitized+': '+gender + emoji +" sûr à " + res.data.accuracy + "%"
