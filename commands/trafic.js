d5e0050447f54a799529c4af49d537921572c31e
Barbe-Bleue <screy@icloud.com>
2018-12-12 19:11:40 +0100

    nice update trafic function

--- commands/trafic.js
+++ commands/trafic.js
@@ -4,34 +4,43 @@ const Discord = require('discord.js');
 module.exports = async args => {	
 	let infoTrafic = "";
 	let statusMessage;
+	let type;
+	
+	if(args[0] === "metros" || args[0] === "metro") {
+		type = "metros"
+	} else if (args[0] === "rers" || args[0] === "rer") {
+		type = "rers"
+	}
+	
+	let res = await axios.get("https://api-ratp.pierre-grimaud.fr/v3/traffic/"+type);
+	
 	if(args.length === 1) {
-		let res = await axios.get("https://api-ratp.pierre-grimaud.fr/v3/traffic/"+args[0]);
-		if(args[0] === "metros" || args[0] === "metro" ) {
+		if(type === "metros") {
 			res.data.result.metros.forEach(item => {
-				if(item.title === "Trafic normal") statusMessage = (":white_check_mark: : "+item.message);
-				else if(item.title === "Travaux") statusMessage = (":warning: : "+item.message);
-				else if(item.title === "Trafic perturbé") statusMessage = (":octagonal_sign: : "+item.message);
-				else if (item.title === "Trafic très perturbé") statusMessage = (":poop: : " +item.message);
+				statusMessage = getStatusMessage(item.title,item.message);
 				infoTrafic += "Ligne **"+item.line+"**: "+statusMessage+"\n";
 			})
-		} else if(args[0] === "rers" || args[0] === "rer") {
+		} else if(type === "rers") {
 			res.data.result.rers.forEach(item => {
-				if(item.title === "Trafic normal") statusMessage = (":white_check_mark: : "+item.message);
-				else if(item.title === "Travaux") statusMessage = (":warning: : "+item.message);
-				else if(item.title === "Trafic perturbé") statusMessage = (":octagonal_sign: : "+item.message);
-				else if (item.title === "Trafic très perturbé") statusMessage = (":poop: : " +item.message);
+				statusMessage = getStatusMessage(item.title,item.message);
 				infoTrafic += "Ligne **"+item.line+"**: "+statusMessage+"\n";
 			})
 		}	
 	} else if(args.length == 2) {
-		let res = await axios.get("https://api-ratp.pierre-grimaud.fr/v3/traffic/"+args[0]+"/"+args[1]);
-		let statusMessage;
-		if(res.data.result.title === "Trafic normal") statusMessage = (":white_check_mark: : "+	res.data.result.message);
-		else if(res.data.result.title === "Travaux") statusMessage = (":warning: : "+	res.data.result.message);
-		else if(res.data.result.title === "Trafic perturbé") statusMessage = (":octagonal_sign: : "+item.message);
-		else if (res.data.result.title === "Trafic très perturbé") statusMessage = (":poop: : " +	res.data.result.message);
+		let res = await axios.get("https://api-ratp.pierre-grimaud.fr/v3/traffic/"+type+"/"+args[1]);
+		statusMessage = getStatusMessage(res.data.result.title,res.data.result.message);
 		infoTrafic += "Ligne **"+	res.data.result.line+"**: "+statusMessage+"\n";
 	}
+	
+	function getStatusMessage(title, message) {
+		let statusMessage;
+		if(title === "Trafic normal") statusMessage = (":white_check_mark: : "+message);
+		else if(title === "Travaux") statusMessage = (":warning: : "+message);
+		else if(title === "Trafic perturbé") statusMessage = (":octagonal_sign: : "+message);
+		else if (title === "Trafic très perturbé") statusMessage = (":poop: : " +message);
+		return statusMessage
+	}
+	
 	return (new Discord.RichEmbed()
 		.setTitle("Info traffic")
 		.setColor(0x4AC1AE)
