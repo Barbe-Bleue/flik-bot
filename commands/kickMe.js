96ac3cc9ee3c318c213b0f8ad6016de21b14b30f
Barbe-Bleue <screy@icloud.com>
2018-12-13 17:53:22 +0100

    update kickMe function

--- commands/kickMe.js
+++ commands/kickMe.js
@@ -1,7 +1,7 @@
 module.exports = message => {
 	if (!message.author.kickable) {
 		message.reply("Je peux pas te kick t'es admin.");
-	} else{
+	} else {
 		message.reply("ok.").then(() => message.member.kick());
 	}
 }
