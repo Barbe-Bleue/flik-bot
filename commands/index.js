7fe89e3e5900a0ed9fb930ff2f2dba5b3f14723b
Barbe-Bleue <screy@icloud.com>
2018-12-13 18:24:23 +0100

    move commands

--- commands/index.js
+++ commands/index.js
@@ -1,36 +1,47 @@
 var exports = module.exports = {};
 
 exports.decide = require("./decide.js")
-exports.gif =  require('./gif.js')
 exports.gender = require("./gender.js")
-exports.doc = require("./help/doc.js")
 exports.weather = require("./weather/weather.js")
 exports.break = require("./break/break.js")
 exports.topGame = require("./topGame.js")
 exports.picture = require("./picture.js")
 exports.chuck = require("./chuck.js")
-exports.wikipedia = require("./wikipedia.js")
-exports.amazon = require("./amazon.js")
 exports.trafic = require("./trafic.js")
-exports.knowledge = require("./brain/knowledge.js")
-exports.brain = require("./brain/brain.js")
-exports.writeBrain = require("./brain/writeBrain.js")
 exports.beauf = require("./beauf/beauf.js")
 exports.translate = require("./translate/translate.js")
-exports.insult = require("./insult/insult.js")
-exports.police = require("./insult/police.js")
 exports.news = require("./news.js")
 exports.coin = require("./coin.js")
-exports.cat = require("./cat.js")
-exports.catFact = require("./catFact.js")
-exports.help = require("./help/help.js")
 exports.kick = require("./kick.js")
 exports.countdown = require("./countdown.js")
 exports.roulette = require("./roulette/roulette.js")
 exports.strawpall = require("./strawpall.js")
 exports.kickMe = require("./kickMe.js")
 exports.rename = require("./rename.js")
+
+// cat 
+exports.cat = require("./cat/cat.js")
+exports.catFact = require("./cat/catFact.js")
+
+// help
+exports.help = require("./help/help.js")
+exports.doc = require("./help/doc.js")
+
+// detector
+exports.insult = require("./insult/insult.js")
+exports.police = require("./insult/police.js")
+
+// brain 
+exports.knowledge = require("./brain/knowledge.js")
+exports.brain = require("./brain/brain.js")
+exports.writeBrain = require("./brain/writeBrain.js")
+
+// search command 
 exports.google = require("./google.js")
+exports.wikipedia = require("./wikipedia.js")
+exports.gif =  require('./gif.js')
+exports.amazon = require("./amazon.js")
+
 // admin command
 exports.ban = require("./admin/ban.js")
 exports.mute = require("./admin/mute.js")
