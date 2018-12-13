5dd2ff3a75984ba085d63f227d4b2ad149e48872
Barbe-Bleue <screy@icloud.com>
2018-12-13 18:19:14 +0100

    update google function

--- commands/google.js
+++ commands/google.js
@@ -1,30 +1,44 @@
-const axios = require("axios");
 const Discord = require('discord.js');
+const awaitMessagesOptions = require("../config.json").awaitMessagesOptions
+const errorMessage = require("../config.json").errorMessage
 const google = require("google");
 const googleResults =  require("../config.json").googleResults
 
-module.exports = async (message,args) => {
-	let search = args.join(' ');
+module.exports = (message,args) => {
 	google.l
-	google.resultsPerPage = googleResults;
+	google.resultsPerPage = googleResults+1;
 	google.lang = 'fr';
 	google.tld = 'fr';
 	google.nextText='Plus';
 	google.protocol = 'https';
-
- 	await google(search, (err,data ) => {
-		data.links.forEach(link => {
-			if(link.title != "") {
-				message.reply(new Discord.RichEmbed()
-				.setTitle(link.title)
-				.setColor(0x4285F4)
-				.setDescription(link.description)
-				.setThumbnail("http://diylogodesigns.com/blog/wp-content/uploads/2016/04/google-logo-icon-PNG-Transparent-Background.png")
-				.setURL(link.href))
-			}		 
+	
+	if (args.length >= 1) {
+		getGoogleResults(args.join('-'))
+	} else if (args.length == 0) {
+		message.reply('tu veux quoi ?').then(() => {
+			message.channel.awaitMessages(response => response.content.length > 0 ,awaitMessagesOptions)
+			.then(collected => {
+				console.log();
+				getGoogleResults(collected.first().content)
+			}).catch(() => {
+				message.reply(errorMessage.waitingToMuch);
+			});
 		});
-	})
+	}
 	
-	
- 
+ 	 async function getGoogleResults(search) {
+		 await google(search, (err,data ) => {
+		 data.links.forEach(link => {
+			 console.log(link);
+			 if(link.href) {
+				 message.reply(new Discord.RichEmbed()
+				 .setTitle(link.title)
+				 .setColor(0x4285F4)
+				 .setDescription(link.description)
+				 .setThumbnail("http://diylogodesigns.com/blog/wp-content/uploads/2016/04/google-logo-icon-PNG-Transparent-Background.png")
+				 .setURL(link.href))
+			 }		 
+		 });
+	 })	
+	}
 }
